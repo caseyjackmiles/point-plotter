e175abce8f8a37877ec3fee877b8a4ad5f77f882
Jack Miles <caseyjackmiles@gmail.com>
2014-12-13 01:58:34 +0000

    Add basic prompt for expr

--- point-plotter.hs
+++ point-plotter.hs
@@ -3,6 +3,7 @@
 -- Dec. 12, 2014
 ----------------
 
+import System.IO -- for hFlush function
 
 -- Represent polynomials as tuples
 -- of coefficients and exponents
@@ -15,5 +16,20 @@ data Expr = GThan [PolyTerm] | LThan [PolyTerm] deriving Show
 
 -- Do nothing for now
 main :: IO ()
-main = return ()
+main = do
+    inp <- promptForExpr
+    putStrLn ("Your expression: " ++ inp)
+
+
+-- From HaskellWiki: Intro to Haskell IO Actions
+promptLine :: String -> IO String
+promptLine prompt = do
+    putStr prompt
+    hFlush stdout
+    getLine
+
+-- Specialize prompt for equation input
+promptForExpr :: IO String
+promptForExpr = 
+    promptLine "\nEnter comparison operator and [(Coefficient, Exponent)] values\ny "
 
