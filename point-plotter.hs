8865e32b72c7ef9e90eb2964abd44283da47165a
Jack Miles <caseyjackmiles@gmail.com>
2014-12-13 05:13:04 +0000

    Add Maybe to message parse

--- point-plotter.hs
+++ point-plotter.hs
@@ -3,6 +3,10 @@
 -- Dec. 12, 2014
 ----------------
 
+module Main where
+
+import Data.Maybe
+import Text.Read -- for readMaybe function
 import System.IO -- for hFlush function
 
 -- Represent polynomials as tuples
@@ -14,22 +18,37 @@ type PolyTerm = (Float, Int)
 data Expr = GThan [PolyTerm] | LThan [PolyTerm] deriving Show
 
 
--- Do nothing for now
+
+-- Prompt for expression input, print out if
+-- successfully parsed
 main :: IO ()
 main = do
     inp <- promptForExpr
-    putStrLn ("Your expression: " ++ inp)
+    case parseExpr inp of
+        Nothing -> print "NOTHING!"
+        Just e -> print e
 
 
 -- From HaskellWiki: Intro to Haskell IO Actions
 promptLine :: String -> IO String
-promptLine prompt = do
-    putStr prompt
-    hFlush stdout
-    getLine
+promptLine prompt = putStr prompt >> hFlush stdout >> getLine
 
 -- Specialize prompt for equation input
 promptForExpr :: IO String
 promptForExpr = 
     promptLine "\nEnter comparison operator and [(Coefficient, Exponent)] values\ny "
+
+
+-- Easy parsing of expression
+parseExpr :: String -> Maybe Expr
+parseExpr (a:b:terms) = case readTermsMaybe terms of
+    Just (t:ts) -> if (a:b:[]) == "<=" 
+        then Just $ LThan (t:ts)
+        else Just $ GThan (t:ts)
+    Just [] -> Nothing
+    Nothing -> Nothing
+
+
+readTermsMaybe :: String -> Maybe [PolyTerm]
+readTermsMaybe str = readMaybe str
 
